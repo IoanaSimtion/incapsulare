c58c48c34d548081f96d9be4f6305ec8b0dc2f1f
Ioana Simtion <ionitza6@gmail.com>
2023-04-12 18:24:11 +0300

    added  table of cars

---
+++ incapsulare/model/Masina.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incapsulare.model
+{
+    internal class Masina
+    {
+        //todo :5 proprietati
+        private string marca;
+        private string model;
+        private int an;
+        private double pret;
+        private string id;
+        private string vechime;
+        private string stare;
+        private string cutieViteze;
+
+        public Masina(string marca,string model, int an, double pret, string id, string vechime, string stare, string cutieViteze)
+        {
+            this.marca = marca;
+            this.model = model;
+            this.an = an;
+            this.pret = pret;
+            this.id = id;
+            this.vechime = vechime;
+            this.stare = stare;
+            this.cutieViteze = cutieViteze;
+        }
+
+
+        public string descriere()
+        {
+            string text = "";
+
+            text += "Marca este " + marca + "\n";
+            text += "Modelul este " + model + "\n";
+            text += "Anul este " + an + "\n";
+            text += "Pretul este " + pret + "\n";
+            text += "ID-ul este " + id + "\n";
+            text += "Masina este " + vechime + "\n";
+            text += "Masina este " + stare + "\n";
+            text += "Cutia de viteze este " + cutieViteze + "\n";
+            return text;
+        }
+
+        //get set
+
+
+        public void setMarca(String marca)
+        {
+            this.marca = marca;
+        }
+
+        public String getMarca()
+        {
+
+            return this.marca;
+        }
+
+        public void setModel(String model)
+        {
+            this.model = model;
+        }
+
+        public String getModel()
+        {
+            return this.model;
+        }
+
+        public void setAn(int an)
+        {
+            this.an = an;
+        }
+
+        public int getAn()
+        {
+            return this.an;
+        }
+
+        public void setPret(double pret)
+        {
+            this.pret = pret;
+        }
+
+        public double getPret()
+        {
+            return this.pret;
+        }
+
+        public void setID(String id)
+        {
+            this.id = id;
+        }
+
+        public String getID()
+        {
+            return this.id;
+        }
+
+        public void setVechime(String vechime)
+        {
+            this.vechime = vechime;
+        }
+
+        public String getVechime()
+        {
+            return this.vechime;
+        }
+
+        public void setStare(String stare)
+        {
+            this.stare = stare;
+        }
+
+        public String getStare()
+        {
+            return this.stare;
+        }
+
+        public void setCutieViteze(String cutieViteze)
+        {
+            this.cutieViteze = cutieViteze;
+        }
+
+        public String getCutieViteze()
+        {
+            return this.cutieViteze;
+        }
+    }
+}
---
+++ incapsulare/view/Home.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace incapsulare.view
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMasini = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstMasini
+            // 
+            this.lstMasini.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lstMasini.GridLines = true;
+            this.lstMasini.Location = new System.Drawing.Point(25, 44);
+            this.lstMasini.Name = "lstMasini";
+            this.lstMasini.Size = new System.Drawing.Size(754, 316);
+            this.lstMasini.TabIndex = 0;
+            this.lstMasini.UseCompatibleStateImageBehavior = false;
+            this.lstMasini.View = System.Windows.Forms.View.Details;
+            this.lstMasini.SelectedIndexChanged += new System.EventHandler(this.lstMasini_SelectedIndexChanged);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMasini);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListView lstMasini;
+    }
+}
